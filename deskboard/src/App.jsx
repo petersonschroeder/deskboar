0215c8a750afdf3c7d5d66de5fa70eb99630a584
PetersonSchroeder <pepirocha123@gmail.com>
2025-08-21 16:43:10 -0300

    segundo commit parte visual da interface de login em breve conclusão

--- deskboard/src/App.jsx
+++ deskboard/src/App.jsx
@@ -1,13 +1,20 @@
 import React from "react"
 import './styles/global.scss'
+import { Login } from "./pages/Login";
 
 function App() {
-  
-
   return (
-    <>
-     
-    </>
+    <div className="App">
+      <div className="container">
+        <div >
+          <h1>Bem-vindo!</h1>
+          <p>Informações, imagem ou ilustração aqui.</p>
+        </div>
+        <div className="direita" >
+          <Login />
+        </div>
+      </div>
+    </div>
   )
 }
 
--- deskboard/src/pages/Login.jsx
+++ deskboard/src/pages/Login.jsx
@@ -1,16 +1,37 @@
 import { useState } from "react";   
 
 export function Login(){ 
-    const [ Login , setLogin] = useState(null)
-    const [ senha, setsenha] = useState(null)
+    const [login, setLogin] = useState("");
+    const [senha, setSenha] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // evita reload da página
+        console.log("Login:", login, "Senha:", senha);
+        // aqui você pode chamar sua API de login
+    };
 
     return (
-        <>
-        <form >
-            <input type="text"  placeholder="Login"/>
-            <input type="password"  name="senha"/>
-            <button type="submit">Login</button>
-        </form>
-        </>
+        <div >
+           
+            <form onSubmit={handleSubmit}>
+                <h2 className="sing">sing in </h2>
+                <input 
+                    type="text" 
+                    placeholder="Login" 
+                    value={login} 
+                    onChange={(e) => setLogin(e.target.value)}
+                />
+                <input 
+                    type="password"  
+                    placeholder="Senha"
+                    value={senha}
+                    onChange={(e) => setSenha(e.target.value)}
+                />
+                <h3 className="SENHA" >Esqueceu sua senha?</h3>
+                <button type="submit" className="buttonlogin">Login</button>
+                <h2>  </h2>
+                <h3 className="opcao"> or continue with</h3>
+            </form>
+        </div>
     )
 }
