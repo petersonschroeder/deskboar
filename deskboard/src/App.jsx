9cf1171c1c992d3a5d8fc039cddef42965add8e2
Peterson Rocha Schroeder <m148464@ACAD.UNISC.BR>
2025-08-25 20:01:21 -0300

    tentativa meio frustada de melhorar a responsividade da login page

--- deskboard/src/App.jsx
+++ deskboard/src/App.jsx
@@ -5,24 +5,25 @@ import { Login } from "./pages/Login";
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <div className="container2">
-        <div>
-          <img src="logo.jpg" alt=""  className="Asepag"/>
-          <h1 className="comentario"><b>sign in to</b></h1>
-          <h2>Loren ip is simply</h2>
-          <p>If you don’t have an account register You can   Register here !</p>
-        </div>
-      
-        <div><img src="boneco.png" alt="" className="boneco"/></div>
-        </div>
- 
-      <div className="direita" >
-          <Login />
-        </div>
+    <div className="container">
+      <div className="esquerda">
+        <img src="logo.jpg" alt="#" className="logo" />
+        <h1 className="titulo"><b>sign in to</b></h1>
+        <h2 className="subtitulo">Loren ip is simply</h2>
+        <p className="descricao">
+          If you don’t have an account register<br />
+          You can <a href="/register">Register here</a>!
+        </p>
+        
+      </div>
+
+      <div className="direita">
+      <img src="boneco.png" alt="#" className="ilustracao" />
+        <Login />
       </div>
     </div>
-  )
+  </div>
+);
 }
 
 export default App
